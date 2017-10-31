dcc891a17effe75fa110cf352fea226db11b3a2c
tkeesling <tyler.keesling@gmail.com>
2017-10-31 11:02:19 -0600

    overlay added

--- src/components/InGame.js
+++ src/components/InGame.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { updatePlayerStats } from '../actions'
+import { Redirect } from 'react-router-dom'
 import { Button, ToggleButtonGroup, ToggleButton } from 'react-bootstrap'
 import Spinner from 'react-spinkit'
 import NavBar from './NavBar'
@@ -12,8 +13,10 @@ class InGame extends Component {
     this.state = {
       outCount: 0,
       rbi: 0,
-      hitType: 'out'
+      hitType: 'out',
+      fireRedirect: false
     }
+
   }
 
   findBatterName(index) {
@@ -21,10 +24,6 @@ class InGame extends Component {
       return player.id === this.props.gameRoster[index].player_id ? player : null
     })
     return batter[0].name
-  }
-
-  getHitType(event) {
-    this.setState({})
   }
 
   addRBI() {
@@ -39,15 +38,47 @@ class InGame extends Component {
     else (this.setState({ rbi: rbiCount - 1 } ))
   }
 
+  resetOuts() {
+    this.setState({ outCount: 0 })
+  }
+
   submitStats() {
-    console.log('hello');
-    let test = {
-	    "id": 23,
-	    "at_bat": 1,
-	    "single": 1
+    let updatedStats = {
+      'id': this.props.gameRoster[0].id,
+      'at_bat': this.props.gameRoster[0].at_bat + 1,
+      'rbi': this.props.gameRoster[0].rbi + this.state.rbi
     }
-    console.log(test);
-    this.props.updatePlayerStats(test)
+    switch(this.state.hitType) {
+      case 'out':
+        this.setState({
+          outCount: this.state.outCount + 1,
+          rbi: 0
+        })
+        return this.props.updatePlayerStats(updatedStats)
+      case 'single':
+        this.setState({ rbi: 0 })
+        updatedStats.single = this.props.gameRoster[0].single + 1
+        return this.props.updatePlayerStats(updatedStats)
+      case 'double':
+        this.setState({ rbi: 0 })
+        updatedStats.double = this.props.gameRoster[0].double + 1
+        return this.props.updatePlayerStats(updatedStats)
+      case 'triple':
+        this.setState({ rbi: 0 })
+        updatedStats.triple = this.props.gameRoster[0].triple + 1
+        return this.props.updatePlayerStats(updatedStats)
+      case 'homerun':
+        this.setState({ rbi: 0 })
+        updatedStats.homerun = this.props.gameRoster[0].homerun + 1
+        return this.props.updatePlayerStats(updatedStats)
+      case 'walk':
+        this.setState({ rbi: 0 })
+        updatedStats.walk = this.props.gameRoster[0].walk + 1
+        updatedStats.at_bat = this.props.gameRoster[0].at_bat - 1
+        return this.props.updatePlayerStats(updatedStats)
+      default:
+      return null
+    }
   }
 
   render() {
@@ -87,7 +118,8 @@ class InGame extends Component {
                 <ToggleButton className='hit-button' value='single'>Single</ToggleButton>
                 <ToggleButton className='hit-button' value='double'>Double</ToggleButton>
                 <ToggleButton className='hit-button' value='triple'>Triple</ToggleButton>
-                <ToggleButton className='hit-button homerun-btn' value='homerun'>Homerun</ToggleButton>
+                <ToggleButton className='hit-button' value='homerun'>Homerun</ToggleButton>
+                <ToggleButton className='hit-button' value='walk'>Walk</ToggleButton>
                 <ToggleButton className='hit-button' value='out'>Out</ToggleButton>
               </ToggleButtonGroup>
             </div>
@@ -114,6 +146,28 @@ class InGame extends Component {
               Next Batter
             </Button>
           </div>
+          {
+            this.state.outCount >= 3 ?
+            <div className="modal-box">
+              <h4>3 outs!</h4>
+              <p>Waiting for the next inning...</p>
+              <Button
+                bsStyle='success'
+                onClick={ () => this.resetOuts() }>
+                Start Next Inning
+              </Button>
+              <Button
+                bsStyle='danger'
+                onClick={ () => this.setState({ fireRedirect: true}) }>
+                End Game
+              </Button>
+            </div>
+            :
+              <div></div>
+          }
+          {
+            this.state.fireRedirect && (<Redirect to='/gamestats' />)
+          }
         </div>
       )
     }
@@ -125,19 +179,3 @@ function mapStateToProps(state) {
 }
 
 export default connect(mapStateToProps, { updatePlayerStats })(InGame)
-
-
-// <div className='rbi-box'>
-//   <span className='hit-selection-text'>RBI(s):</span>
-//   <div className='rbi-count-box'>
-//     <Button
-//       onClick={ () => this.subtractRBI() }
-//       className='rbi-btn'
-//     >-</Button>
-//   <div className='rbi-text'>{ this.state.rbi }</div>
-//     <Button
-//       onClick={ () => this.addRBI() }
-//       className='rbi-btn'
-//     >+</Button>
-//   </div>
-// </div>
